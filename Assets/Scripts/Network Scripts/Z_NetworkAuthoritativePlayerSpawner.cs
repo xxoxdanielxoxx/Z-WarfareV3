593fa6467024cd654e6388445e5a8938b4e8c723
unknown <student@AI251LAB0201P04.aicala.edmc.stu>
2014-10-09 16:37:31 -0700

    10/9/14 Add: Game Manager   Add: authortative Spawner   Notes: the Game Manager is going to be responsible for all Initialization will be the One Spot access to get Scripts. They Are Not yet implemtneted they are in a work in progress

---
+++ Assets/Scripts/Network Scripts/Z_NetworkAuthoritativePlayerSpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Z_NetworkAuthoritativePlayerSpawner : Photon.MonoBehaviour 
+{
+
+	/// <summary>
+	/// Initialize the specified isMasterClient.
+	/// </summary>
+	/// <param name="isMasterClient">If set to <c>true</c> is master client.</param>
+	public void Initialize(bool isMasterClient)
+	{
+		if (isMasterClient)
+		{
+				
+		}
+	}
+
+	public void NetworkSpawnAllPlayers()
+	{
+
+	}
+
+	public void NetworkPlayer()
+	{
+
+	}
+
+	public void LocalPlayers()
+	{
+
+	}
+
+}
---
+++ Assets/Scripts/Z_GameManager.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Z_GameManager : Photon.MonoBehaviour 
+{
+	bool m_bOnlinePlay;
+
+	static Z_GameManager s_GameManager;
+
+	//Network Initialization
+	public Z_Netowork_PlayerSpawner m_NetPlayerSpawner;
+	public Z_NetworkAuthoritativePlayerSpawner m_ZNet_AuthPlayerSpawner;
+
+	PhotonView  m_PhotonView;
+
+
+	// Set To Defualt to true, just in case we are in offline mode
+	bool m_bIsHost = true;
+
+	public int m_iNumOfPlayers = 0;
+
+	void Awake()
+	{
+		Initialize ();
+	}
+
+	void Initialize()
+	{
+		m_NetPlayerSpawner = GetComponent<Z_Netowork_PlayerSpawner> ();
+		m_ZNet_AuthPlayerSpawner = GetComponent<Z_NetworkAuthoritativePlayerSpawner> ();
+
+		m_PhotonView = GetComponent<PhotonView> ();
+	
+
+		//Online Check
+		if (m_PhotonView == null)
+			OnlineModeOff();	
+		else
+			OnlineModeOn();
+
+		//Do the Ready Check and Assign the Host
+		if(IsOnline())
+		{
+			m_bIsHost = PhotonNetwork.isMasterClient;
+
+
+			if(m_bIsHost)
+			{
+				ReadyCheck();
+			}
+			else
+			{
+				m_PhotonView.RPC("ReadyCheck",PhotonTargets.MasterClient);	
+			}
+
+			//Start Spawning Players
+			m_ZNet_AuthPlayerSpawner.Initialize(m_bIsHost);
+		
+		}
+		else
+		{
+			//Activate Non Network Player Spawner
+		}
+	}
+
+
+	[RPC]
+	void ReadyCheck()
+	{
+		m_iNumOfPlayers++;
+		
+		if (m_iNumOfPlayers == PhotonNetwork.countOfPlayers) 
+		{
+			photonView.RPC ("SpawnLocalPlayer", PhotonTargets.OthersBuffered);
+			SpawnLocalPlayer();
+			
+			AIMaster m = (AIMaster)FindObjectOfType(typeof(AIMaster));
+			StartCoroutine( m.StartAI());
+		}
+	} 
+
+	public bool IsHost()
+	{
+		return m_bIsHost;
+	}
+
+	public void OnlineModeOff()
+	{
+		m_bOnlinePlay = false;
+	}
+
+	public void OnlineModeOn()
+	{
+		m_bOnlinePlay = true;
+	}
+
+	public bool IsOnline()
+	{
+		return m_bOnlinePlay; 
+	}
+
+
+
+	/// <summary>
+	/// Get this Single Static instance.
+	/// </summary>
+	public static Z_GameManager Get()
+	{
+		return s_GameManager;
+	}
+}
